2216f578690507aa096a955716ecca6159834407
sinourain <sinourain@gmail.com>
2013-09-28 12:13:14 -0500

    generate mongoid config and devise install with common User model

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,4 +61,6 @@ TwitterForZombies::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
+  config.assets.initialize_on_precompile = false
+
 end
